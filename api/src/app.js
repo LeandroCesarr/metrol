3a9f49164a0ada6711f30ea92143e883600f4b9d
Leandro C. Silva <leandro.cesar@novahaus.com.br>
2019-09-13 17:45:16 -0300

    Create procfile

--- api/src/app.js
+++ api/src/app.js
@@ -3,9 +3,9 @@ const notifier = require('node-notifier');
 const nomralizePort = require('./config/port');
 const router = require('./routes/router');
 const bodyParser = require('body-parser');
-const database = require('./config/database');
+// const database = require('./config/database');
 const logger = require('morgan');
-database('mongodb://localhost:27017/metrol');
+// database('mongodb://localhost:27017/metrol');
 
 const app = express();
 const port = nomralizePort(process.env.PORT || '4000');
