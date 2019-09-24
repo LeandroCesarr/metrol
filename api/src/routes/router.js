c20825da8b2b7d8f6e5d4101d129b8ba09f59c4f
Leandro C. Silva <leandrocesar49@hotmail.com>
2019-09-24 19:20:22 -0300

    Create category controller

--- api/src/routes/router.js
+++ api/src/routes/router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const repairShop = require('../controllers/repairShopController');
 const service = require('../controllers/serviceController');
 const user = require('../controllers/userController');
+const category = require('../controllers/categoryController');
 
 router.get('/repair-shop', repairShop.indexOf);
 router.get('/repair-shop', repairShop.index);
@@ -11,6 +12,11 @@ router.put('/repair-shop', repairShop.update);
 router.post('/repair-shop', repairShop.create);
 router.delete('/repair-shop', repairShop.delete);
 
+router.get('/user/:id', user.indexOf);
+router.get('/user', user.index);
+router.put('/user', user.update);
+router.post('/user', user.create);
+router.delete('/user', user.delete);
 
 router.get('/service/:id', service.indexOf);
 router.get('/service', service.index);
@@ -18,10 +24,10 @@ router.put('/service', service.update);
 router.post('/service', service.create);
 router.delete('/service', service.delete);
 
-router.get('/user/:id', user.indexOf);
-router.get('/user', user.index);
-router.put('/user', user.update);
-router.post('/user', user.create);
-router.delete('/user', user.delete);
+router.get('/category/:id', category.indexOf);
+router.get('/category', category.index);
+router.put('/category', category.update);
+router.post('/category', category.create);
+router.delete('/category', category.delete);
 
 module.exports = router;
